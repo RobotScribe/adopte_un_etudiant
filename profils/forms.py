76d58110fe493dbca4784fbfab758de73f3ba885
qsomervi <quentin.somerville@telecom-bretagne.eu>
2016-11-22 22:59:16 +0100

    inscriptions faites

--- profils/forms.py
+++ profils/forms.py
@@ -7,9 +7,9 @@ class InscriptionForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     age = forms.IntegerField()
-    photo = forms.ImageField(required=False)#l'ajout doit être optionnel
-    description = forms.CharField(required=False,label="Description (facultative)")#l'ajout doit être optionnel
-    is_student = forms.BooleanField(required=False,label = "cochez si vous êtes étudiant")#ça doit être optionnel de cochez la case
-    école = forms.CharField(required=False,label = "rentrez votre école si vous êtes étudiant")#doit etre optionnel
+    photo = forms.ImageField(required=False)
+    description = forms.CharField(required=False,label="Description (facultative)")
+    is_student = forms.BooleanField(required=False,label = "cochez si vous êtes étudiant")
+    école = forms.CharField(required=False,label = "rentrez votre école si vous êtes étudiant")
     
 
