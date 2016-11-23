2854c180b53363df88ecff6537fc340703d2734e
qsomervi <quentin.somerville@telecom-bretagne.eu>
2016-11-23 18:12:19 +0100

    test

--- profils/views.py
+++ profils/views.py
@@ -95,6 +95,8 @@ def list_annonces(request):
 def annonce(resquest,numero) :
     annonce = get_object_or_404(Annonce, numero=numero)
     return render(request,'annonce.html',locals())
+
+
     
 def proposer_annonce(request):
     pass
