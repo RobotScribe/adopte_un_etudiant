1ad911ccaaa4af8bfbac12bf081e06f47a1129bc
qsomervi <quentin.somerville@telecom-bretagne.eu>
2016-11-23 15:23:11 +0100

    compte pl créé

--- profils/admin.py
+++ profils/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from profils.models import Profil
 
-class ProfilAdmin(admin.ModelAdmin):
+#class ProfilAdmin(admin.ModelAdmin):
     #list_display = ('user.username', 'user.email', 'is_student', 'user.last-login', 'user.date_joines')
-    list_filter = ('is_student',)#,'is_active') 
+    #list_filter = ('is_student',)#,'is_active') 
     #search_fields = ('user.username', 'user.first_name', 'user.last_name', 'user.email')
 
-admin.site.register(Profil, ProfilAdmin)
+admin.site.register(Profil)#, ProfilAdmin)
