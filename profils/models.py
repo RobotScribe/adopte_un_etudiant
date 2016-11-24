bcd9edde3e921cff9d54301a3f6d3c224710f551
qsomervi <quentin.somerville@telecom-bretagne.eu>
2016-11-24 21:31:04 +0100

    continuation

--- profils/models.py
+++ profils/models.py
@@ -38,10 +38,13 @@ class Annonce (models.Model) :
 	annonce = models.TextField(null=False)
 	date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name = "Date de dépôt")
 	prix = models.DecimalField(max_digits=5,decimal_places=2)
-	annonceur=models.ForeignKey("Profil")
+	annonceur = models.CharField(max_length=100)
+	etudiant = models.CharField(max_length=100, null=True)
 	lieux = models.CharField(max_length=50)
 	distance_max = models.IntegerField()
 	numero = models.IntegerField()
+	#etat ne peux prendre que 3 valeurs : "a faire", "fait", "en cours"
+	etat = models.CharField(max_length=10)
 
 	def __str__(self) :
 		return self.titre 
